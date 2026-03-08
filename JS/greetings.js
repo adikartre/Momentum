6229c4a9b95b977a624fd5fcb2203798ab9225ff
adikartre <adikartre@gmail.com>
2026-03-08 23:20:33 +0900

    로그아웃 버튼 생성

--- JS/greetings.js
+++ JS/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutBtn = document.querySelector("#logout-btn");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,18 +9,27 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
    event.preventDefault();
    loginForm.classList.add(HIDDEN_CLASSNAME);
-   
+
    const username = loginInput.value;
    localStorage.setItem(USERNAME_KEY, username);
    paingtGreetings(username);
-
 }
 
 function paingtGreetings(username){
    greeting.innerText = `Hello ${username}`;
    greeting.classList.remove(HIDDEN_CLASSNAME);
+   logoutBtn.classList.remove(HIDDEN_CLASSNAME);
 }
 
+function onLogout() {
+   localStorage.removeItem(USERNAME_KEY);
+   greeting.classList.add(HIDDEN_CLASSNAME);
+   logoutBtn.classList.add(HIDDEN_CLASSNAME);
+   loginInput.value = "";
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+}
+
+logoutBtn.addEventListener("click", onLogout);
 
 const localStorageUsername = localStorage.getItem(USERNAME_KEY);
 
